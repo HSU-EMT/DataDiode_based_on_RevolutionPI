35b4bc80f9c84d3e565c176811d6a3b9e004584f
songha <songsonkitty@gmail.com>
2022-08-09 09:44:56 +0200

    fix address range

--- applications/core0/src/shared_memory.h
+++ applications/core0/src/shared_memory.h
@@ -12,42 +12,39 @@
 #include "custom_definitions.h"
 
 
-//#define COUNT			(*(volatile unsigned long *)(SHARED_OCM_BASE_ADDRESS))    // 4 byte
-//#define ARRAY			((volatile unsigned char *)(SHARED_OCM_BASE_ADDRESS + 4)) // 1 byte
-
 //memory parts of 10 module, each owns 200 bytes for request and response packets, which should enough for all module when they drives fully functions
 #define MEM_REQ_1F			((u8 *)(SHARED_OCM_BASE_ADDRESS))       // 0-99
-#define MEM_RES_1F			((u8 *)(SHARED_OCM_BASE_ADDRESS + 100)) // 100-199
-#define MEM_REQ_1E			((u8 *)(SHARED_OCM_BASE_ADDRESS + 200)) // 200-299
-#define MEM_RES_1E			((u8 *)(SHARED_OCM_BASE_ADDRESS + 300)) // 300-399
-#define MEM_REQ_1D			((u8 *)(SHARED_OCM_BASE_ADDRESS + 400)) // 400-499
-#define MEM_RES_1D			((u8 *)(SHARED_OCM_BASE_ADDRESS + 500)) // 500-599
-#define MEM_REQ_1C			((u8 *)(SHARED_OCM_BASE_ADDRESS + 600)) // 600-699
-#define MEM_RES_1C			((u8 *)(SHARED_OCM_BASE_ADDRESS + 700)) // 700-799
-#define MEM_REQ_1B			((u8 *)(SHARED_OCM_BASE_ADDRESS + 800)) // 800-899
-#define MEM_RES_1B			((u8 *)(SHARED_OCM_BASE_ADDRESS + 900)) // 900-999
-#define MEM_REQ_20			((u8 *)(SHARED_OCM_BASE_ADDRESS + 1000)) // 1000-1099
-#define MEM_RES_20			((u8 *)(SHARED_OCM_BASE_ADDRESS + 1100)) // 1100-1199
-#define MEM_REQ_21			((u8 *)(SHARED_OCM_BASE_ADDRESS + 1200)) // 1200-1299
-#define MEM_RES_21			((u8 *)(SHARED_OCM_BASE_ADDRESS + 1300)) // 1300-1399
-#define MEM_REQ_22			((u8 *)(SHARED_OCM_BASE_ADDRESS + 1400)) // 1400-1499
-#define MEM_RES_22			((u8 *)(SHARED_OCM_BASE_ADDRESS + 1500)) // 1500-1599
-#define MEM_REQ_23			((u8 *)(SHARED_OCM_BASE_ADDRESS + 1600)) // 1600-1699
-#define MEM_RES_23			((u8 *)(SHARED_OCM_BASE_ADDRESS + 1700)) // 1700-1799
-#define MEM_REQ_24			((u8 *)(SHARED_OCM_BASE_ADDRESS + 1800)) // 1800-1899
-#define MEM_RES_24			((u8 *)(SHARED_OCM_BASE_ADDRESS + 1900)) // 1900-1999
+#define MEM_RES_1F			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x64)) // 100-199
+#define MEM_REQ_1E			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0xC8)) // 200-299
+#define MEM_RES_1E			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x12C)) // 300-399
+#define MEM_REQ_1D			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x190)) // 400-499
+#define MEM_RES_1D			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x1F4)) // 500-599
+#define MEM_REQ_1C			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x258)) // 600-699
+#define MEM_RES_1C			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x2BC)) // 700-799
+#define MEM_REQ_1B			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x320)) // 800-899
+#define MEM_RES_1B			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x384)) // 900-999
+#define MEM_REQ_20			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x3E8)) // 1000-1099
+#define MEM_RES_20			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x44C)) // 1100-1199
+#define MEM_REQ_21			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x4B0)) // 1200-1299
+#define MEM_RES_21			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x514)) // 1300-1399
+#define MEM_REQ_22			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x578)) // 1400-1499
+#define MEM_RES_22			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x5DC)) // 1500-1599
+#define MEM_REQ_23			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x640)) // 1600-1699
+#define MEM_RES_23			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x6A4)) // 1700-1799
+#define MEM_REQ_24			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x708)) // 1800-1899
+#define MEM_RES_24			((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x76C)) // 1900-1999
 
 
 
 //memory parts of device information for 10 module, each reserves 100 bytes
-#define MEM_DEVICE_INFOR	((u8 *)(SHARED_OCM_BASE_ADDRESS + 2000))       // 2000-2999
+#define MEM_DEVICE_INFOR	((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x7D0))       // 2000-2999
 //memory parts of device number, one byte only
-#define MEM_DEVICE_NUM  	((int *)(SHARED_OCM_BASE_ADDRESS + 3000))      // 3000
+#define MEM_DEVICE_NUM  	((int *)SHARED_OCM_BASE_ADDRESS+ 0xBB8))      // 3000
 //memory to save configuration request and response of all devices
-#define MEM_DEVICE_CONF_REQ     ((u8 *)(SHARED_OCM_BASE_ADDRESS + 3100))       //3100-6099, because each can use up to 300 bytes maximal
-#define MEM_DEVICE_CONF_RES     ((u8 *)(SHARED_OCM_BASE_ADDRESS + 6100))       //6100-6399, because each can use up to 30 bytes maximal
+#define MEM_DEVICE_CONF_REQ     ((u8 *)SHARED_OCM_BASE_ADDRESS+ 0xC1C))       //3100-6099, because each can use up to 300 bytes maximal
+#define MEM_DEVICE_CONF_RES     ((u8 *)SHARED_OCM_BASE_ADDRESS+ 0x17D4))       //6100-6399, because each can use up to 30 bytes maximal
 //memory parts of configuration packet number, one byte only
-#define MEM_DEVICE_CONF_NUM  	((int *)(SHARED_OCM_BASE_ADDRESS + 6200))      // 6200
+#define MEM_DEVICE_CONF_NUM  	((int *)SHARED_OCM_BASE_ADDRESS+ 0x1838))      // 6200
 #define MAX_BYTE			6201
 
 
